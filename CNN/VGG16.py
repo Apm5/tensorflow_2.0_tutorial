472136b662c8a6f1ad227c4dbb368afa9c5e6df0
Apm5 <lijiakun112@gmail.com>
2019-10-11 23:27:33 +0800

    VGG for Cifar-10

---
+++ CNN/VGG16.py
@@ -0,0 +1,113 @@
+import tensorflow as tf
+import numpy as np
+import pickle as p
+import os
+from tensorflow.keras import models, optimizers, regularizers
+from tensorflow.keras.callbacks import LearningRateScheduler
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+weight_decay = 5e-4
+batch_size = 128
+learning_rate = 1e-2
+dropout_rate = 0.5
+epoch_num = 50
+
+def load_CIFAR_batch(filename):
+    """ load single batch of cifar """
+    with open(filename, 'rb')as f:
+        datadict = p.load(f, encoding='iso-8859-1')
+        X = datadict['data']
+        Y = datadict['labels']
+        X = X.reshape(10000, 3, 32, 32)
+        Y = np.array(Y)
+        return X, Y
+
+
+def load_CIFAR(Foldername):
+    train_data = np.zeros([50000, 32, 32, 3], dtype=np.float32)
+    train_label = np.zeros([50000, 10], dtype=np.float32)
+    test_data = np.zeros([10000, 32, 32, 3], dtype=np.float32)
+    test_label = np.zeros([10000, 10], dtype=np.float32)
+
+    for sample in range(5):
+        X, Y = load_CIFAR_batch(Foldername + "/data_batch_" + str(sample + 1))
+
+        for i in range(3):
+            train_data[10000 * sample:10000 * (sample + 1), :, :, i] = X[:, i, :, :]
+        for i in range(10000):
+            train_label[i + 10000 * sample][Y[i]] = 1
+
+    X, Y = load_CIFAR_batch(Foldername + "/test_batch")
+    for i in range(3):
+        test_data[:, :, :, i] = X[:, i, :, :]
+    for i in range(10000):
+        test_label[i][Y[i]] = 1
+
+    return train_data, train_label, test_data, test_label
+
+def VGG16():
+    model = models.Sequential()
+    model.add(Conv2D(64, (3, 3), activation='relu', padding='same', input_shape=(32, 32, 3), kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(Conv2D(64, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(MaxPooling2D((2, 2)))
+
+    model.add(Conv2D(128, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(Conv2D(128, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(MaxPooling2D((2, 2)))
+
+    model.add(Conv2D(256, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(Conv2D(256, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(Conv2D(256, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(MaxPooling2D((2, 2)))
+
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(MaxPooling2D((2, 2)))
+
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+
+    model.add(Flatten())  # 2*2*512
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(10, activation='softmax'))
+
+    return model
+
+
+def scheduler(epoch):
+    if epoch < epoch_num * 0.4:
+        return learning_rate
+    if epoch < epoch_num * 0.8:
+        return learning_rate * 0.1
+    return learning_rate * 0.01
+
+
+if __name__ == '__main__':
+    # gpu config
+    physical_devices = tf.config.experimental.list_physical_devices('GPU')
+    tf.config.experimental.set_memory_growth(device=physical_devices[0], enable=True)
+
+    # load data
+    (train_images, train_labels, test_images, test_labels) = load_CIFAR('/home/user/Documents/dataset/Cifar-10')
+
+    # get model
+    model = VGG16()
+
+    # show
+    model.summary()
+
+    # train
+    sgd = optimizers.SGD(lr=learning_rate, momentum=0.9, nesterov=True)
+    change_lr = LearningRateScheduler(scheduler)
+    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    model.fit(train_images, train_labels,
+              batch_size=batch_size,
+              epochs=epoch_num,
+              callbacks=[change_lr],
+              validation_data=(test_images, test_labels))
